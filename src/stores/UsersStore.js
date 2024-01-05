74f48f8be21ed478d07ef77c19dec44ff66c54ed
Nermin Donat <nermindonat@posta.mu.edu.tr>
2024-01-05 15:34:44 +0300

    Users store

--- src/stores/UsersStore.js
+++ src/stores/UsersStore.js
@@ -1,7 +1,5 @@
 import { defineStore } from "pinia";
 import axios from "axios";
-// import serenadImg from "../assets/images/serenad.jpg";
-// import bulbulImg from "../assets/images/bülbül.jpeg";
 
 export const useUsersStore = defineStore("userStore", {
   // the part where the data is kept
@@ -16,7 +14,6 @@ export const useUsersStore = defineStore("userStore", {
           "https://jsonplaceholder.typicode.com/users"
         );
         this.users = data.data;
-        console.log(this.users);
       } catch (error) {
         alert(error);
         console.log(error);
